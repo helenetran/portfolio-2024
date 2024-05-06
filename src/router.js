02c12726b548b70c82d0374ed7aa6a9c68b4d8e2
helenetran <helene.andre.06@gmail.com>
2024-05-06 21:36:25 +0200

    Update routes.

--- src/router.js
+++ src/router.js
@@ -2,10 +2,14 @@ import { createMemoryHistory, createRouter } from 'vue-router'
 
 import Home from './components/Home.vue'
 import About from './components/About.vue'
+import Works from './components/Works.vue'
+import GetInTouch from './components/GetInTouch.vue'
 
 const routes = [
   { path: '/', component: Home },
   { path: '/about', component: About },
+  { path: '/works', component: Works },
+  { path: '/get-in-touch', component: GetInTouch },
 ]
 
 export const router = createRouter({
