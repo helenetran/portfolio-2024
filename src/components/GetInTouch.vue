8dc590829e68097ace656d20e9c0b56c9fc20f3a
helenetran <helene.andre.06@gmail.com>
2024-05-06 21:36:55 +0200

    Create views: get in touch, works.

---
+++ src/components/GetInTouch.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Get in Touch</h1>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/Works.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Works</h1>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
