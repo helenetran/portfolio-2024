09beb0066e8610402344df887e88ba9be9bb715b
helenetran <helene.andre.06@gmail.com>
2024-05-08 22:49:39 +0200

    Add left and right columns to the page.

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,9 @@ import Menu from './components/Menu.vue'
     <Menu></Menu>
   </header>
 
+  <div class="side-bar left"></div>
+  <div class="side-bar right"></div>
+
   <main>
     <RouterView />
   </main>
@@ -15,7 +18,7 @@ import Menu from './components/Menu.vue'
   <footer></footer>
 </template>
 
-<style>
+<style lang="scss">
 * {
   box-sizing: border-box;
   padding: 0;
@@ -27,7 +30,33 @@ body {
   min-height: 100dvh;
   padding: 50px;
   padding-top: 100px;
-  background-color: #f5ded5;
+  background-color: #f5ece6;
+  color: #444346;
+  background: #f5ece6;  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #f2d8ce, #f5ece6);  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #f2d8ce, #f5ece6); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+}
+
+main {
+  width: 70vw;
+  padding-top: 100px;
+  margin: auto;
+}
+
+.side-bar {
+  border: 1px solid var(--border);
+  width: 15vw;
+  height: 100dvh;
+  position: fixed;
+  top: 0;
+
+  &.left {
+    left: 0;
+  }
+
+  &.right {
+    right: 0;
+  }
 }
 
 </style>
