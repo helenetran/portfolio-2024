2cdc47779adeb56d212cec94b00f95b27f02ad97
helenetran <helene.andre.06@gmail.com>
2024-05-19 16:16:04 +0200

    Style the footer.

---
+++ src/components/Footer.vue
@@ -0,0 +1,41 @@
+<script>
+
+</script>
+
+<template lang="pug">
+  footer.footer
+    div.contact
+      div.contact__text Let's talk :)
+      div Email LinkedIn
+    div.copyright Copyright @ 2024 Helene Tran. All rights reserved.
+
+</template>
+
+<style lang="scss">
+.footer {
+  position: absolute;
+  left: 0;
+  height: 10vh;
+  width: 100vw;
+  text-align: center;
+
+  .contact {
+    display: flex;
+    justify-content: center;
+    width: 100vw;
+
+    &__text {
+      padding-right: 10px;
+      font-family: CaviarDreamsBold;
+    }
+  }
+
+  .copyright {
+    opacity: 0.6;
+    font-style: italic;
+    font-size: 13px;
+    padding-top: 20px;
+  }
+}
+
+</style>
