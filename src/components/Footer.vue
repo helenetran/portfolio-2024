ef33d1ead7bfa9fd7fe7a571648ebaa1e2a1484d
helenetran <helene.andre.06@gmail.com>
2024-05-24 21:47:50 +0200

    Minor footer style improvements.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,8 +6,8 @@
   footer.footer
     div.contact
       .contact__text Let's talk :)
-      a.i-linkedin(href="https://www.linkedin.com/in/helenetrandev/" target="_blank")
-      a.i-github(href="https://github.com/helenetran" target="_blank")
+      a.contact__icon.i-linkedin(href="https://www.linkedin.com/in/helenetrandev/" target="_blank")
+      a.contact__icon.i-github(href="https://github.com/helenetran" target="_blank")
     div.copyright Copyright @ 2024 Helene Tran. All rights reserved.
 
 </template>
@@ -29,13 +29,18 @@
       padding-right: 10px;
       font-family: CaviarDreamsBold;
     }
+
+    &__icon {
+      font-size: 18px;
+      padding-left: 5px;
+    }
   }
 
   .copyright {
     opacity: 0.6;
     font-style: italic;
     font-size: 13px;
-    padding-top: 20px;
+    padding-top: 12px;
   }
 }
 
