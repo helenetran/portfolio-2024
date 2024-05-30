f08e877bb493c301b1041ec72e599ca9b785d0f4
helenetran <helene.andre.06@gmail.com>
2024-05-30 10:55:40 +0200

    Create projects skeleton.

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,8 +1,40 @@
 <template lang="pug">
 div
   h1 Projects
+  div.projects
+    .project
+      .project__description.description
+        .description__client Client name
+        .description__title Doing this and that
+        .description__website www.lorem.com
+        .description__keywords css | javascript
+      .project__image image here
+
 </template>
 
 <style lang="scss">
+.projects {
+  padding: 20px;
 
+  .project {
+    display: flex;
+    justify-content: space-between;
+
+    &__image {min-width: 66%;}
+    .description {
+      div {padding-bottom: 5px;}
+
+      &__client {
+        color: var(--secondary-color);
+        font-style: italic;
+        font-family: CaviarDreamsBold;
+        font-size: 14px;
+      }
+
+      &__title {
+        font-family: 'CaviarDreamsBold';
+      }
+    }
+  }
+}
 </style>
