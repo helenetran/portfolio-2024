d5b8f32ab09234fee05a70aefe8504c631c78c49
helenetran <helene.andre.06@gmail.com>
2024-06-10 13:29:26 +0200

    Clean up.

--- src/components/GetInTouch.vue
+++ src/components/GetInTouch.vue
@@ -4,15 +4,14 @@ div
 
   form(action='https://formspree.io/f/mkgwwelz' method='POST')
     label
-        | Your Name:
-        input(type='text' name='text' required)
+      | Your Name:
+      input(type='text' name='text' required)
     label
       | Your email:
       input(type='email' name='email' required)
     label
       | Your message:
       textarea(name='message' required)
-    // your other form fields go here
     button(type='submit') Send
 </template>
 
