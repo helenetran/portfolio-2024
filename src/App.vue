ca6313b5eb3cab7d2d379ee99a92da5adc31ecdc
helenetran <helene.andre.06@gmail.com>
2024-06-11 13:41:20 +0200

    Clean up.

--- src/App.vue
+++ src/App.vue
@@ -41,9 +41,8 @@ body {
   min-height: 100dvh;
   padding: 50px;
   padding-top: 100px;
-  background-color: #f5ece6;
   color: #444346;
-  background: #f5ece6;  /* fallback for old browsers */
+  background-color: #f5ece6;
   background: -webkit-linear-gradient(to right, #f2d8ce, #f5ece6);  /* Chrome 10-25, Safari 5.1-6 */
   background: linear-gradient(to right, #f2d8ce, #f5ece6); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 }
