217312d38e6edd2b85f6800bfed3af0431282bba
helenetran <helene.andre.06@gmail.com>
2024-06-13 09:10:19 +0200

    Add about me text in the page.

--- src/components/About.vue
+++ src/components/About.vue
@@ -8,34 +8,42 @@ onMounted (() => {
 </script>
 
 <template lang="pug">
-div
+.about
   h1 About
-  .i-hotdog
 
-  div
-    div Hi,
-    br
-    div I’m Helene, a web developer who loves creating beautiful and functional digital experiences. I've worked a lot with Vue.js and Shopify, developing everything from e-commerce sites to interactive data visualizations. I enjoy bringing ideas to life and making sure they are not only visually appealing but also user-friendly and responsive.
-    br
-    div I take pride in my attention to detail and my commitment to crafting seamless user interfaces. When I'm not coding, I enjoy keeping up with the latest tech trends, mixing up some fun cocktails, and sharing knowledge with the developer community.
-    br
-    div Outside of tech, I'm passionate about cooking and eating delicious food. I love experimenting with new recipes and exploring different cuisines. If you want to see know more about my cooking hobby:&nbsp;
-      a(href="www.eatingwithhelene.com" target="_blank") eatingwithhelene.com&nbsp;
-      span ;)
-    div When I'm not in the kitchen, you might find me gliding through the streets on my inline roller skates, enjoying the freedom and thrill of the ride.
-    br
-    div I'm always excited to take on new challenges and help bring innovative web solutions to life. So, if you're looking for someone who is passionate about web development and dedicated to creating engaging digital experiences, I'd love to chat!
+  .about__description
+    div
+      h2 Hi there!
+      br
+      div I’m Helene, a web developer who loves creating beautiful and functional digital experiences. I've worked a lot with Vue.js and Shopify, developing everything from e-commerce sites to interactive data visualizations. I enjoy bringing ideas to life and making sure they are not only visually appealing but also user-friendly and responsive.
+      br
+      div I take pride in my attention to detail and my commitment to crafting seamless user interfaces. When I'm not coding, I enjoy keeping up with the latest tech trends, mixing up some fun cocktails, and sharing knowledge with the developer community.
+      br
+      div Outside of tech, I'm passionate about cooking and eating delicious food. I love experimenting with new recipes and exploring different cuisines. If you want to see know more about my cooking hobby:&nbsp;
+        a(href="www.eatingwithhelene.com" target="_blank") eatingwithhelene.com&nbsp;
+        span ;)
+      div When I'm not in the kitchen, you might find me gliding through the streets on my inline roller skates, enjoying the freedom and thrill of the ride.
+      br
+      div I'm always excited to take on new challenges and help bring innovative web solutions to life. So, if you're looking for someone who is passionate about web development and dedicated to creating engaging digital experiences, I'd love to chat!
+
+    div
+      h2 Other things I enjoy
+      div
 
 </template>
 
-<style lang="scss" scoped>
-#about-page {
-  // background: #f9f6e8;
-  background: white;
+<style lang="scss">
+html #about-page {
+  background: #f5f0eb;
 
-  a {
-    font-style: italic;
-    color: var(--secondary-color);
+  .i-hotdog {color: var(--secondary-color);}
+  .about__description {
+    padding-left: 10px;
+
+    a {
+      font-style: italic;
+      color: var(--secondary-color);
+    }
   }
 }
 </style>
