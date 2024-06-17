c49058d8249bbed1640b3bb7b4099d4570a8bb65
helenetran <helene.andre.06@gmail.com>
2024-06-17 11:12:38 +0200

    Changes imgaes in projects page.

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -13,53 +13,53 @@ const projects = [
     websiteName: 'framar.com',
     website: 'https://framar.com',
     keywords: 'Shopify | javascript | css',
-    image: new URL('@/assets/projects-images/framar.png', import.meta.url).href },
+    image: new URL('@/assets/projects-images/framar.webp', import.meta.url).href },
   {
     client: 'Everhome',
     title: 'Fully customized e-commerce website developed using Shopify, featuring advanced integrations, responsive design, and enhanced user experience.',
     websiteName: 'everhome.co',
-    website: 'https://everhome.co', keywords: 'Shopify | javascript | css', image: new URL('@/assets/projects-images/everhome.png', import.meta.url).href },
+    website: 'https://everhome.co', keywords: 'Shopify | javascript | css', image: new URL('@/assets/projects-images/everhome.webp', import.meta.url).href },
   {
     client: 'Credibility Institute',
     title: 'Dynamic web app for data visualization and statistical analysis using Vue.js & Firebase, featuring interactive graphs and real-time updates.',
     websiteName: 'evaluation361.com',
     website: 'www.evaluation361.com',
-    keywords: 'Vue.js | javascript | css  | firebase', image: new URL('@/assets/projects-images/evaluation361.png', import.meta.url).href },
+    keywords: 'Vue.js | javascript | css  | firebase', image: new URL('@/assets/projects-images/evaluation361.webp', import.meta.url).href },
   {
     client: 'Groupe Chênevert',
     title: 'Elegant website developed using Vue.js, featuring a responsive design and smooth user interactions.',
     websiteName: 'groupechenevert.com',
     website: 'www.groupechenevert.com',
     keywords: 'Vue.js | javascript | css',
-    image: new URL('@/assets/projects-images/groupe-chenevert.png', import.meta.url).href },
+    image: new URL('@/assets/projects-images/groupe-chenevert.webp', import.meta.url).href },
   {
     client: 'Vanderbrand',
     title: 'Sophisticated showcase website developed using Vue.js.',
     websiteName: 'onedelisle.com',
     website: 'https://onedelisle.com',
     keywords: 'Vue.js | javascript | css',
-    image: new URL('@/assets/projects-images/onedelisle.png', import.meta.url).href },
+    image: new URL('@/assets/projects-images/onedelisle.webp', import.meta.url).href },
   {
     client: 'Superhumans',
     title: 'Showcase website developed with Vue.js, featuring GSAP animations and an engaging user experience.',
     websiteName: 'superhumans.it',
     website: 'https://superhumans.it',
     keywords: 'Vue.js | javascript | css',
-    image: new URL('@/assets/projects-images/superhumans.png', import.meta.url).href },
+    image: new URL('@/assets/projects-images/superhumans.webp', import.meta.url).href },
   {
     client: 'Sweet Cocktails',
     title: 'Dynamic hobby website about coooocktails using Vue.js and PHP, offering a vibrant user interface and interactive recipe exploration.',
     websiteName: 'sweetcocktails.com',
     website: 'https://sweetcocktails.com',
     keywords: 'Vue.js | javascript | css | PHP',
-    image: new URL('@/assets/projects-images/sweetcocktails.png', import.meta.url).href },
+    image: new URL('@/assets/projects-images/sweetcocktails.webp', import.meta.url).href },
   {
     client: 'Eating with Helene :)',
     title: 'In my kitchen, What I do for fun.',
     websiteName: 'eatingwithhelene.com',
     website: 'https://eatingwithhelene.com',
     keywords: 'Vue.js | javascript | css | PHP',
-    image: new URL('@/assets/projects-images/sweetcocktails.png', import.meta.url).href }
+    image: new URL('@/assets/projects-images/sweetcocktails.webp', import.meta.url).href }
 ]
 </script>
 
