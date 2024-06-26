b814308e50b8d6ea0c0e67b4d110e407ad52a650
helenetran <helene.andre.06@gmail.com>
2024-06-26 12:17:31 +0200

    Create contact form & link to formspree.

--- src/components/GetInTouch.vue
+++ src/components/GetInTouch.vue
@@ -1,8 +1,56 @@
 <template lang="pug">
 div
   h1 Get in Touch
+
+  form(action='https://formspree.io/f/mkgwwelz' method='POST')
+    label
+        | Your Name:
+        input(type='text' name='text')
+    label
+      | Your email:
+      input(type='email' name='email')
+    label
+      | Your message:
+      textarea(name='message')
+    // your other form fields go here
+    button(type='submit') Send
 </template>
 
 <style lang="scss">
+form {
+  display: flex;
+  flex-direction: column;
 
+  label {
+    display: flex;
+    flex-direction: column;
+    padding: 10px;
+  }
+
+  textarea {
+    height: 150px;
+    resize: none;
+
+    &:focus-visible {outline: none;}
+  }
+
+  input, textarea {
+    border: none;
+    margin-top: 10px;
+    padding: 10px;
+  }
+  button {
+    align-self: flex-end;
+    margin-right: 10px;
+    padding: 5px 10px;
+    background-color: var(--secondary-color-light);
+    color: #fff;
+    border: none;
+    transition: 0.3s ease-in-out;
+
+    &:hover {
+      background-color: var(--secondary-color-dark);
+    }
+  }
+}
 </style>
